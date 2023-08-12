e7f9e940f7a2d93741183a3578fdb2bdfa1bcf1e
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-12 09:01:20 -0700

    style test

--- console.py
+++ console.py
@@ -1,15 +1,12 @@
 #!usr/bin/python3
-"""
-
-Defines the AirBnB Console
-
-"""
+"""Defines the AirBnB Console"""
+import cmd
 
 
-class HBNBCommand:
+class HBNBCommand(cmd.Cmd):
     """Defines the command interpreter"""
     pass
 
 
 if __name__ == "__main__":
-    HBNBCommand()
+    HBNBCommand().cmdloop()
