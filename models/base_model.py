7e0bb4ad0602538acd0f42bfdab46dfc2449e87e
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-12 13:02:21 -0700

    model storage

--- models/base_model.py
+++ models/base_model.py
@@ -18,24 +18,15 @@ class BaseModel:
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         timeform = "%Y-%m-%dT%H:%M:%S.%f"
-        if len(kwargs) != 0:
-            for k, val in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(val, timeform)
-                else:
-                    self.__dict__[k] = val
-        else:
-            models.storage.new(self)
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
         name = self.__class__.__name__
-        return "[{}] ({}) {}".format(name, self.id, self.__dict__)
+        return f"[{name}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.today()
-        models.storage.save()
 
     def to_dict(self):
         """Return a dictionary containing all keys/values of
