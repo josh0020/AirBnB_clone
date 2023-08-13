ce6ad06af74bbcca20a978c3df6ad91a540388dd
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-13 14:04:55 -0700

    console 0.1

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ import models
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 import re
 import shlex
 from datetime import datetime
