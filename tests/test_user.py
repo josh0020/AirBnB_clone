744123a1fd795baf9cf3bb595eec4143ecabda51
josh <joshuaking34111573@gmail.com>
2023-08-13 14:50:39 -0700

    test

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for test State class"""
+"""Module for test User class"""
 import unittest
 import json
 import pep8
@@ -13,43 +13,46 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.insert(0, parent_dir)
 
-from models.state import State
+from models.user import User
 from models.base_model import BaseModel
 
 
-class TestState(unittest.TestCase):
-    """Test State class implementation"""
+class TestUser(unittest.TestCase):
+    """Test User class implementation"""
     def test_doc_module(self):
         """Module documentation"""
-        doc = State.__doc__
+        doc = User.__doc__
         self.assertGreater(len(doc), 1)
 
-    def test_pep8_conformance_state(self):
-        """Test that models/state.py conforms to PEP8."""
+    def test_pep8_conformance_base_model(self):
+        """Test that models/user.py conforms to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/state.py'])
+        result = pep8style.check_files(['models/user.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_pep8_conformance_test_state(self):
-        """Test that tests/test_models/test_state.py conforms to PEP8."""
+    def test_pep8_conformance_test_base_model(self):
+        """Test that tests/test_models/test_user.py conforms to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        res = pep8style.check_files(['tests/test_models/test_state.py'])
+        res = pep8style.check_files(['tests/test_models/test_user.py'])
         self.assertEqual(res.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_doc_constructor(self):
         """Constructor documentation"""
-        doc = State.__init__.__doc__
+        doc = User.__init__.__doc__
         self.assertGreater(len(doc), 1)
 
     def test_class(self):
         """Validate the types of the attributes an class"""
         with self.subTest(msg='Inheritance'):
-            self.assertTrue(issubclass(State, BaseModel))
+            self.assertTrue(issubclass(User, BaseModel))
 
         with self.subTest(msg='Attributes'):
-            self.assertIsInstance(State.name, str)
+            self.assertIsInstance(User.email, str)
+            self.assertIsInstance(User.password, str)
+            self.assertIsInstance(User.first_name, str)
+            self.assertIsInstance(User.last_name, str)
 
 if __name__ == '__main__':
     unittest.main()
